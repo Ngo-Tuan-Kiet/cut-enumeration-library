abf30ba16f40ec1e9076119e9dcce683a42c057a
Florian Kühn <kyuhn.florian@gmail.com>
2024-05-02 20:09:56 +0200

    Reworked 'get_original_partition' and removed 'get_all_leaf_vectors'
    'get_original_partition' now uses actual nodes, which fixed some incorrect outputs. 'get_all_leaf_vectors' blows up when tested with an example molecule, so I removed it (wasn't useful anyway). Algorithm now seems to work properly.

--- src/varizani_yannakakis.py
+++ src/varizani_yannakakis.py
@@ -13,7 +13,6 @@ class Cut:
         self.partition = data['partition']
         self.partition_vector = data['partition_vector']
         self.mother = data['mother']
-        self.leaf_set = data['leaf_set']
 
     def __lt__(self, other):
         return self.value < other.value
@@ -33,13 +32,6 @@ def cut_to_vector(G: nx.DiGraph, cut: tuple[int | float, tuple[set, set]]) -> st
             vector += '1'
 
     return vector
-
-
-def get_all_leaf_vectors(n: int, internal_vector='') -> list[str]:
-    """
-    Given an integer n and an optional internal node vector, return all possible leaf vectors of length n.
-    """
-    return [internal_vector + ''.join(seq) for seq in itertools.product("01", repeat=n - len(internal_vector))]
 
 
 def get_immediate_children(internal_vector: str, leaf_vector: str) -> list[str]:
@@ -143,9 +135,9 @@ def contract_nodes_with_edge_addition(G: nx.DiGraph, u: int | str, v: int | str,
     return G_collapsed
 
 
-def collapse_graph(G: nx.DiGraph, cut_vector: str) -> nx.DiGraph:
+def collapse_graph(G: nx.DiGraph, mother: str) -> nx.DiGraph:
     """
-    Given a directed graph G and a cut, represented by its binary vector, return the collapsed graph.
+    Given a directed graph G and a mother node, represented by its binary vector, return the collapsed graph.
     """
     if G.is_directed() is False:
         raise ValueError('The graph must be directed to use this function.')
@@ -154,10 +146,10 @@ def collapse_graph(G: nx.DiGraph, cut_vector: str) -> nx.DiGraph:
     nodes = list(G.nodes)
     S = []
     T = []
-    for i in range(len(cut_vector)):
-        if cut_vector[i] == '0':
+    for i in range(len(mother)):
+        if mother[i] == '0':
             S.append(nodes[i])
-        elif cut_vector[i] == '1':
+        elif mother[i] == '1':
             T.append(nodes[i])
 
     # Collapse the nodes in S and T
@@ -174,24 +166,32 @@ def collapse_graph(G: nx.DiGraph, cut_vector: str) -> nx.DiGraph:
     return G_collapsed
 
 
-def get_original_partition(partition: tuple[set, set], cut_vector: str) -> tuple[set, set]:
+def get_original_partition(G: nx.DiGraph, partition: tuple[set, set], mother: str) -> tuple[set, set]:
     """
-    Given a partition of nodes including S and T nodes and the cut vector used to collapse, return the original partition of nodes.
+    Given a graph G, a partition with collapsed nodes 'S' and 'T' and mother vector that was used to collapse the graph, return the partition with the original nodes.
     """
+    # Copy the collapsed partition
     original_partition = partition
-    # Go through the cut vector and assign each node to its original side
-    for i in range(len(cut_vector)):
-        if cut_vector[i] == '0':
-            original_partition[0].add(i+1)
-        elif cut_vector[i] == '1':
-            original_partition[1].add(i+1)
-    
-    # Remove the S and T nodes from the partition
+
+    # Separate the nodes into sets S and T
+    nodes = list(G.nodes)
+    S = []
+    T = []
+    for i in range(len(mother)):
+        if mother[i] == '0':
+            S.append(nodes[i])
+        elif mother[i] == '1':
+            T.append(nodes[i])
+
+    # Remove the S and T nodes from the partition and replace them with the original nodes
     for side in original_partition:
         if 'S' in side:
             side.remove('S')
+            side.update(S)
         if 'T' in side:
             side.remove('T')
+            side.update(T)
+
     return original_partition
 
 
@@ -199,15 +199,14 @@ def varizani_yannakakis_directed(G: nx.DiGraph) -> list[tuple[int | float, tuple
     """
     Varizani-Yannakakis algorithm for enumerating all min-cuts of a graph G.
     """
-    enumerated_cuts = []
+    # enumerated_cuts = []
     # Calculate the global min cut of the graph and get necessary data
     min_cut_value, min_cut_partition = global_min_cut(G)
     min_cut_vector = cut_to_vector(G, min_cut_partition)
-    leaf_set = get_all_leaf_vectors(G.number_of_nodes())
     
     # Initialize priority queue with the min cut value, it's node partition, the mother vector and all possible leaf vectors
     queue = PriorityQueue()
-    queue.put(Cut(min_cut_value, {'partition': min_cut_partition, 'partition_vector': min_cut_vector, 'mother': '', 'leaf_set': leaf_set}))
+    queue.put(Cut(min_cut_value, {'partition': min_cut_partition, 'partition_vector': min_cut_vector, 'mother': ''}))
 
     while not queue.empty():
 
@@ -215,7 +214,8 @@ def varizani_yannakakis_directed(G: nx.DiGraph) -> list[tuple[int | float, tuple
         current_cut: Cut = queue.get()
 
         # Add the current cut to the list of enumerated cuts
-        enumerated_cuts.append((current_cut.value, current_cut.partition))
+        # enumerated_cuts.append((current_cut.value, current_cut.partition))
+        print(current_cut.value, current_cut.partition_vector)
 
         # Get the immediate children of the current cut
         immediate_children = get_immediate_children(current_cut.mother, current_cut.partition_vector)
@@ -225,15 +225,14 @@ def varizani_yannakakis_directed(G: nx.DiGraph) -> list[tuple[int | float, tuple
             collapsed_graph = collapse_graph(G, child_vector)
             # Calculate the min cut for the child and get the necessary data
             child_min_value, child_min_partition = partly_specified_min_cut(collapsed_graph)
-            child_min_partition = get_original_partition(child_min_partition, child_vector)
+            child_min_partition = get_original_partition(G, child_min_partition, child_vector)
             child_min_vector = cut_to_vector(G, child_min_partition)
-            child_leaf_set = get_all_leaf_vectors(G.number_of_nodes(), child_vector)
 
             # Add the min cut of the child to the queue
-            queue.put(Cut(child_min_value, {'partition': child_min_partition, 'partition_vector': child_min_vector, 'mother': child_vector, 'leaf_set': child_leaf_set}))
+            queue.put(Cut(child_min_value, {'partition': child_min_partition, 'partition_vector': child_min_vector, 'mother': child_vector}))
 
     # Return the list of enumerated cuts
-    return enumerated_cuts
+    # return enumerated_cuts
 
 
 def varizani_yannakakis(G: nx.DiGraph | nx.Graph) -> list[tuple[int | float, tuple[set, set]]]:
@@ -244,20 +243,27 @@ def varizani_yannakakis(G: nx.DiGraph | nx.Graph) -> list[tuple[int | float, tup
 
 
 if __name__ == '__main__':
-    G = nx.DiGraph()
-    G.add_edge(1, 2, capacity=10)
-    G.add_edge(2, 1, capacity=10)
+    # G = nx.DiGraph()
+    # G.add_edge(1, 2, capacity=10)
+    # G.add_edge(2, 1, capacity=10)
 
-    G.add_edge(2, 3, capacity=10)
-    G.add_edge(3, 2, capacity=10)
+    # G.add_edge(2, 3, capacity=10)
+    # G.add_edge(3, 2, capacity=10)
 
-    G.add_edge(2, 4, capacity=1)
-    G.add_edge(4, 2, capacity=1)
+    # G.add_edge(2, 4, capacity=1)
+    # G.add_edge(4, 2, capacity=1)
 
-    G2 = nx.DiGraph()
-    G2.add_edge(1, 2, capacity=1)
-    G2.add_edge(2, 3, capacity=2)
-    G2.add_edge(3, 1, capacity=3)
+    # G2 = nx.DiGraph()
+    # G2.add_edge(1, 2, capacity=1)
+    # G2.add_edge(2, 3, capacity=2)
+    # G2.add_edge(3, 1, capacity=3)
+
+    G = nx.read_graphml('data/example_molecules/0.graphml')
+
+    # Replace attribute 'order' with 'capacity' for all edges
+    for edge in G.edges:
+        G[edge[0]][edge[1]]['capacity'] = G[edge[0]][edge[1]]['order']
+        del G[edge[0]][edge[1]]['order']
 
     print(varizani_yannakakis(G))
 
