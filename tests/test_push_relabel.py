825dfa269883851c182014f773350eb46063288b
Ngo Tuan Kiet <tuankietngo2002@outlook.de>
2024-05-21 10:58:05 +0200

    merged from florain to ngo deleted copy of deprecated push relabel algo and deleted corresponding tests also wrote tests for future hao orlin implementation

--- tests/test_push_relabel.py
+++ tests/test_push_relabel.py
@@ -1,8 +1,8 @@
 import pytest
 import networkx as nx
 from networkx.algorithms.flow import minimum_cut
-import src.push_relabel_copy as pr_optimized
 import src.push_relabel as pr
+import src.varizani_yannakakis as vy
 
 
 @pytest.mark.parametrize('s, t', [
@@ -43,30 +43,6 @@ def test_push_relabel_star_graph(star_graph, s, t):
     (4, 2),
     (4, 3)
 ])
-def test_push_relabel_optimized_star_graph(star_graph, s, t):
-    G = star_graph
-    G2 = G.copy()
-
-    min_cut = pr_optimized.push_relabel(G, s, t)[0]
-
-    nx_min_cut = minimum_cut(G2, s, t)[0]
-
-    assert min_cut == nx_min_cut
-
-@pytest.mark.parametrize('s, t', [
-    (1, 2),
-    (1, 3),
-    (1, 4),
-    (2, 1),
-    (2, 3),
-    (2, 4),
-    (3, 4),
-    (3, 1),
-    (3, 2),
-    (4, 1),
-    (4, 2),
-    (4, 3)
-])
 def test_push_relabel_complex_graph(complex_graph, s, t):
     G = complex_graph
     G2 = G.copy()
@@ -77,25 +53,23 @@ def test_push_relabel_complex_graph(complex_graph, s, t):
     assert min_cut == nx_min_cut
 
 
-@pytest.mark.parametrize('s, t', [
-    (1, 2),
-    (1, 3),
-    (1, 4),
-    (2, 1),
-    (2, 3),
-    (2, 4),
-    (3, 4),
-    (3, 1),
-    (3, 2),
-    (4, 1),
-    (4, 2),
-    (4, 3)
-])
-def test_push_relabel_optimized_complex_graph(complex_graph, s, t):
-    G = complex_graph
-    G2 = G.copy()
+# @pytest.mark.parametrize('s', [1, 2, 3, 4])
+# def test_hao_orlin_star_graph(star_graph, s):
+#     G = star_graph
+#     G2 = G.copy()
 
-    min_cut = pr_optimized.push_relabel(G, s, t)[0]
+#     min_cut = pr.hao_orlin(G, s)[0]
 
-    nx_min_cut = minimum_cut(G2, s, t)[0]
-    assert min_cut == nx_min_cut
+#     bf_min_cut = vy.minimum_s_cut(G2, s)[0]
+#     assert min_cut == bf_min_cut
+
+
+# @pytest.mark.parametrize('s', [1, 2, 3, 4])
+# def test_hao_orlin_complex_graph(complex_graph, s):
+#     G = complex_graph
+#     G2 = G.copy()
+
+#     min_cut = pr.hao_orlin(G, s)[0]
+
+#     bf_min_cut = vy.minimum_s_cut(G2, s)[0]
+#     assert min_cut == bf_min_cut
