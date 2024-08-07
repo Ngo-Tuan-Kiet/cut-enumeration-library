0dc5b797d7581092903e33d97d3debff2bb5aec5
Ngo Tuan Kiet <tuankietngo2002@outlook.de>
2024-08-07 15:42:55 +0200

    added new yeh solution to branch

--- tests/test_yeh.py
+++ tests/test_yeh.py
@@ -3,7 +3,7 @@ import pytest
 from math import inf
 from src.varizani_yannakakis import varizani_yannakakis, collapse_graph, Cut
 from src.cut_bases import cut_partition_to_edge_partition
-from src.yeh_original import yeh
+from src.yeh_neu import yeh
 
 
 @pytest.mark.parametrize('graph', ['undirected_triangle', 'single_edge_graph', 'single_node_graph', 'complex_graph', 'star_graph', 'icl_weighted_graph', 'networkx_example_weighted_graph', 'undirected_triangle'])
